1d657874f60d59236724d8eda1cddbb0dc803ad4
robotsofi <sofia.k.pardo@gmail.com>
2025-12-03 23:42:21 -0500

    tracing.js updated to use OTLP Exporter

--- tracing.js
+++ tracing.js
@@ -1,37 +1,40 @@
-const { resourceFromAttributes } = require('@opentelemetry/resources');
-const { ATTR_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
-
 const { NodeSDK } = require('@opentelemetry/sdk-node');
-const {
-    getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node');
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
-const {
-    ConsoleMetricExporter,
-    PeriodicExportingMetricReader,
-} = require('@opentelemetry/sdk-metrics');
-
-
-//Instrumentations
-const { ExpressInstrumentation } =
-    require("opentelemetry-instrumentation-express");
-const { MongoDBInstrumentation } =
-    require("@opentelemetry/instrumentation-mongodb");
+const { resourceFromAttributes } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+const { ExpressInstrumentation } = require("opentelemetry-instrumentation-express");
+const { MongoDBInstrumentation } = require("@opentelemetry/instrumentation-mongodb");
 const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 
+const resource = resourceFromAttributes({
+  [SemanticResourceAttributes.SERVICE_NAME]: "todo-service",
+});
+
+const traceExporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
+
+const metricExporter = new OTLPMetricExporter({
+  url: 'http://localhost:4318/v1/metrics',
+});
+
 const sdk = new NodeSDK({
-    resource: resourceFromAttributes({
-        [ATTR_SERVICE_NAME]: "todo-service"
-    }),
-    traceExporter: new ConsoleSpanExporter(),
-    metricReader: new PeriodicExportingMetricReader({
-        exporter: new ConsoleMetricExporter(),
-    }),
-    instrumentations: [
-        getNodeAutoInstrumentations(),
-        new ExpressInstrumentation(),
-        new MongoDBInstrumentation(),
-        new HttpInstrumentation()],
+  resource,
+  traceExporter,
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+    exportIntervalMillis: 60000,
+  }),
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+    new ExpressInstrumentation(),
+    new MongoDBInstrumentation(),
+    new HttpInstrumentation(),
+  ],
 });
 
 sdk.start();
+
